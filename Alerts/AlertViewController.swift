9a9c5b2024fa523525d27f6be6d72591a95d80a1
Dima <dima@Nikitas-iMac.local>
2020-06-25 22:32:07 +0600

    Create AlertView

---
+++ Alerts/AlertViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AlertViewController.swift
+//  Alerts
+//
+//  Created by Dima on 25.06.2020.
+//  Copyright © 2020 Dima. All rights reserved.
+//
+
+import UIKit
+import TinyConstraints
+
+final class AlertViewController: UIViewController {
+    
+   // MARK: - Properties
+    
+    private let alertView = AlertView()
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(alertView)
+        alertView.edgesToSuperview()
+        hideAlert()
+    }
+    
+    // MARK: - Support
+    
+    private func showAlert() {
+        UIView.animate(withDuration: 1) {
+            self.alertView.alpha = 1
+        }
+    }
+    
+    private func hideAlert() {
+        UIView.animate(withDuration: 1, animations: {
+            self.alertView.alpha = 0
+        }, completion: { _ in
+            self.alertView.removeFromSuperview()
+        })
+    }
+}
