2b26343fe61b3fef7266a1df6245223bf7469668
Dima <dima@Nikitas-iMac.local>
2020-06-25 17:21:27 +0600

    Initial commit

---
+++ Alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alerts
+//
+//  Created by Dima on 25.06.2020.
+//  Copyright © 2020 Dima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
